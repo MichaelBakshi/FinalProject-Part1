70b6a4a8f2bbee73f3deb5bbd274dddf5fe4090b
Michael Bakshi <michael.bakshi83@gmail.com>
2021-01-30 12:21:28 +0200

    .

--- FinalProject-Part1/AirlineCompany.cs
+++ FinalProject-Part1/AirlineCompany.cs
@@ -6,5 +6,44 @@ namespace FinalProject_Part1
 {
     class AirlineCompany : IPoco, IUser
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Country_Id { get; set; }
+        public int User_Id { get; set; }
+
+        public AirlineCompany()
+        {
+
+        }
+        public AirlineCompany(string name, int country_Id, int user_Id)
+        {
+            Name = name;
+            Country_Id = country_Id;
+            User_Id = user_Id;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is AirlineCompany company &&
+                   Id == company.Id &&
+                   Name == company.Name &&
+                   Country_Id == company.Country_Id &&
+                   User_Id == company.User_Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, Name, Country_Id, User_Id);
+        }
+
+        public static bool operator ==(AirlineCompany left, AirlineCompany right)
+        {
+            return EqualityComparer<AirlineCompany>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(AirlineCompany left, AirlineCompany right)
+        {
+            return !(left == right);
+        }
     }
 }
