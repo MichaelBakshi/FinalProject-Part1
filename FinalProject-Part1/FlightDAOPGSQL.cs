c7d95281497d2666ff85d12861985f4c165a22f4
Michael Bakshi <michael.bakshi83@gmail.com>
2021-02-09 16:41:47 +0200

    j

--- FinalProject-Part1/FlightDAOPGSQL.cs
+++ FinalProject-Part1/FlightDAOPGSQL.cs
@@ -170,6 +170,105 @@ namespace FinalProject_Part1
             return result;
         }
 
+        public List<Flight> GetFlightsByLandingDate(DateTime _landing_date)
+        {
+            List<Flight> result = new List<Flight>();
+
+            using (NpgsqlCommand cmd = new NpgsqlCommand())
+            {
+                using (cmd.Connection = new NpgsqlConnection(m_conn_string))
+                {
+                    cmd.Connection.Open();
+                    cmd.CommandType = System.Data.CommandType.Text;
+                    cmd.CommandText = $"select * from sp_get_flights_by_departure_date('{_landing_date}')";
+
+                    NpgsqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Flight f = new Flight
+                        {
+                            Id = (int)reader["id"],
+                            Airline_Company_Id = (int)reader["airline_company_id"],
+                            Origin_Country_Id = (int)reader["origin_country_id"],
+                            Destination_Country_Id = (int)reader["destination_country_id"],
+                            Departure_Time = (DateTime)reader["departure_time"],
+                            Landing_Time = (DateTime)reader["landing_time"],
+                            Remaining_Tickets = (int)reader["remaining_tickets"]
+                        };
+                        result.Add(f);
+                    }
+                }
+            }
+            return result;
+        }
+
+        public List<Flight> GetFlightsByDestinationCountry(string _destination_country)
+        {
+            List<Flight> result = new List<Flight>();
+
+            using (NpgsqlCommand cmd = new NpgsqlCommand())
+            {
+                using (cmd.Connection = new NpgsqlConnection(m_conn_string))
+                {
+                    cmd.Connection.Open();
+                    cmd.CommandType = System.Data.CommandType.Text;
+                    cmd.CommandText = $"select * from sp_get_flights_by_destination_country('{_destination_country}')";
+
+                    NpgsqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Flight f = new Flight
+                        {
+                            Id = (int)reader["id"],
+                            Airline_Company_Id = (int)reader["airline_company_id"],
+                            Origin_Country_Id = (int)reader["origin_country_id"],
+                            Destination_Country_Id = (int)reader["destination_country_id"],
+                            Departure_Time = (DateTime)reader["departure_time"],
+                            Landing_Time = (DateTime)reader["landing_time"],
+                            Remaining_Tickets = (int)reader["remaining_tickets"]
+                        };
+                        result.Add(f);
+                    }
+                }
+            }
+            return result;
+        }
+
+        public List<Flight> GetFlightsByOriginCountry(string _origin_country)
+        {
+            List<Flight> result = new List<Flight>();
+
+            using (NpgsqlCommand cmd = new NpgsqlCommand())
+            {
+                using (cmd.Connection = new NpgsqlConnection(m_conn_string))
+                {
+                    cmd.Connection.Open();
+                    cmd.CommandType = System.Data.CommandType.Text;
+                    cmd.CommandText = $"select * from sp_get_flights_by_origin_country('{_origin_country}')";
+
+                    NpgsqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Flight f = new Flight
+                        {
+                            Id = (int)reader["id"],
+                            Airline_Company_Id = (int)reader["airline_company_id"],
+                            Origin_Country_Id = (int)reader["origin_country_id"],
+                            Destination_Country_Id = (int)reader["destination_country_id"],
+                            Departure_Time = (DateTime)reader["departure_time"],
+                            Landing_Time = (DateTime)reader["landing_time"],
+                            Remaining_Tickets = (int)reader["remaining_tickets"]
+                        };
+                        result.Add(f);
+                    }
+                }
+            }
+            return result;
+        }
+
         public void RemoveFlight(int id)
         {
             int result = ExecuteNonQuery($"call  sp_delete_flight ({id})");
