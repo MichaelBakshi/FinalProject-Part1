c9fc4fb06651824a77152cc503603c4c96d1766e
Michael Bakshi <michael.bakshi83@gmail.com>
2021-02-25 20:10:05 +0200

    f

--- FinalProject-Part1/AnonymousUserFacade.cs
+++ FinalProject-Part1/AnonymousUserFacade.cs
@@ -7,13 +7,45 @@ namespace FinalProject_Part1
     public class AnonymousUserFacade : FacadeBase, IAnonymousUserFacade
     {
         
-        public List<AirlineCompany> GetAllAirlines()
-        {
+        //public List<AirlineCompany> GetAllAirlines()
+        //{
             
-            throw new NotImplementedException();
-            //_airlineDAO 
+        //    throw new NotImplementedException();
+        //    //_airlineDAO 
 
+        //}
+        public IList<Flight> GetAllFlights()
+        {
+            throw new NotImplementedException();
         }
-       
+
+        public IList<AirlineCompany> GetAllAirlineCompanies()
+        {
+            throw new NotImplementedException();
+        }
+        public Dictionary<Flight, int> GetAllFlightsVacancy()
+        {
+            throw new NotImplementedException();
+        }
+        public Flight GetFlightById(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public IList<Flight> GetFlightsByOriginCountry(int countryCode)
+        {
+            throw new NotImplementedException();
+        }
+        public IList<Flight> GetFlightsByDestinationCountry(int countryCode)
+        {
+            throw new NotImplementedException();
+        }
+        public IList<Flight> GetFlightsByDepatrureDate(DateTime departureDate)
+        {
+            throw new NotImplementedException();
+        }
+        public IList<Flight> GetFlightsByLandingDate(DateTime landingDate)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ FinalProject-Part1/LoggedInAdministratorFacade.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinalProject_Part1
+{
+    class LoggedInAdministratorFacade : ILoggedInAdministratorFacade, AnonymousUserFacade
+    {
+        public IList<Customer> GetAllCustomers(LoginToken<Administrator> token)
+        {
+            throw new NotImplementedException();
+        }
+        public void CreateNewAirline(LoginToken<Administrator> token, AirlineCompany airline)
+        {
+            throw new NotImplementedException();
+        }
+        public void UpdateAirlineDetails(LoginToken<Administrator> token, AirlineCompany customer)
+        {
+            throw new NotImplementedException();
+        }
+        public void RemoveAirline(LoginToken<Administrator> token, AirlineCompany airline)
+        {
+            throw new NotImplementedException();
+        }
+        public void CreateNewCustomer(LoginToken<Administrator> token, Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+        public void UpdateCustomerDetails(LoginToken<Administrator> token, Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+        public void RemoveCustomer(LoginToken<Administrator> token, Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+        public void CreateAdmin(LoginToken<Administrator> token, Administrator admin)
+        {
+            throw new NotImplementedException();
+        }
+        public void UpdateAdmin(LoginToken<Administrator> token, Administrator admin)
+        {
+            throw new NotImplementedException();
+        }
+        public void RemoveAdmin(LoginToken<Administrator> token, Administrator admin)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
