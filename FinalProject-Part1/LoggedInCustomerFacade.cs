2f6f7b550b06bd90695032b89f63b911ddde002a
Michael Bakshi <michael.bakshi83@gmail.com>
2021-02-25 20:24:42 +0200

    df

---
+++ FinalProject-Part1/LoggedInCustomerFacade.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinalProject_Part1
+{
+    class LoggedInCustomerFacade: AnonymousUserFacade, ILoggedInCustomerFacade
+    {
+        public IList<Flight> GetAllMyFlights(LoginToken<Customer> token)
+        {
+            throw new NotImplementedException();
+        }
+        public Ticket PurchaseTicket(LoginToken<Customer> token, Flight flight)
+        {
+            throw new NotImplementedException();
+        }
+        public void CancelTicket(LoginToken<Customer> token, Ticket ticket)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
