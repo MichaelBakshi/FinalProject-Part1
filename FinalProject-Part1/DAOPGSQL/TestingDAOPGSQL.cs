5a09d85061bf0102af38bbc745b0d1cdeb12c0a9
Michael Bakshi <michael.bakshi83@gmail.com>
2021-04-01 18:14:20 +0300

    j

---
+++ FinalProject-Part1/DAOPGSQL/TestingDAOPGSQL.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinalProject_Part1.DAOPGSQL
+{
+    class TestingDAOPGSQL
+    {
+    }
+}
--- FinalProject-Part1/LogIn/TestingGlobalConfig.cs
+++ FinalProject-Part1/LogIn/TestingGlobalConfig.cs
@@ -1,25 +1,25 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
-using Newtonsoft.Json;
 
-namespace FinalProject_Part1
+namespace FinalProject_Part1.LogIn
 {
-    public class GlobalConfig
+    public class TestingGlobalConfig
     {
         private static ConfigJson configJson;
-        static GlobalConfig()
+        static TestingGlobalConfig()
         {
             string configFile = File.ReadAllText("C:\\Users\\Michael Bakshi\\source\\repos\\FinalProject-Part1\\config.json");
             configJson = JsonConvert.DeserializeObject<ConfigJson>(configFile);
         }
 
-        public static string ConnectionString { get {return configJson.ConnectionString; } }
+        public static string TestConnectionString { get { return configJson.TestConnectionString; } }
     }
 
     class ConfigJson
     {
-        public string ConnectionString { get; set; }
+        public string TestConnectionString { get; set; }
     }
 }
