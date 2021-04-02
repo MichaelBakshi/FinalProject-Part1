3f3ccbb4d92f1133347b2ac1a79514a9de6779a7
Michael Bakshi <michael.bakshi83@gmail.com>
2021-04-02 21:09:59 +0300

    g

--- FinalProjectTestCore/AnonymousTest.cs
+++ FinalProjectTestCore/AnonymousTest.cs
@@ -9,8 +9,7 @@ namespace FinalProjectTestCore
     [TestClass]
     public class AnonymousTest
     {
-        //FlightDAOPGSQL flightDAOPGSQL = new FlightDAOPGSQL();
-        //TestingDAOPGSQL testingDAOPGSQL = new TestingDAOPGSQL();
+
         AnonymousUserFacade anonymousUserFacade = new AnonymousUserFacade();
         TestingFacade testingFacade = new TestingFacade();
 
@@ -20,15 +19,6 @@ namespace FinalProjectTestCore
             testingFacade.ClearDB();
         }
 
-        
-
-        //[TestInitialize]
-        //public void InsertData()
-        //{
-        //    Flight flight = new Flight();
-        //    anonymousUserFacade.GetFlightById();
-            
-        //}
 
         [TestMethod]
         public void GetFlightById()
@@ -51,13 +41,62 @@ namespace FinalProjectTestCore
         [TestMethod]
         public void GetAllAirlineCompanies()
         {
-            
+            AirlineCompany expected_airline_company = new AirlineCompany("airline_name", 3, 2);
+            List<AirlineCompany> list_of_airline_companies = (List < AirlineCompany >) anonymousUserFacade.GetAllAirlineCompanies();
+            List<AirlineCompany> expected_list_of_airlines = null;
+            expected_list_of_airlines.Add(expected_airline_company);
+            Assert.AreEqual(list_of_airline_companies, expected_list_of_airlines);
         }
 
         [TestMethod]
         public void GetAllFlightsVacancy()
         {
-
+            Flight expectedFlight = new Flight(1, 1, 1, DateTime.Now, DateTime.Now, 1);
+            Dictionary<Flight, int> flights_vacancy = anonymousUserFacade.GetAllFlightsVacancy();
+            Dictionary<Flight, int> expected_flights_vacancy = null;
+            expected_flights_vacancy.Add(expectedFlight, 1);
+            Assert.AreEqual(flights_vacancy, expected_flights_vacancy);
         }
+
+        [TestMethod]
+        public void GetFlightsByDepartureDate()
+        {
+            Flight expectedFlight = new Flight(1, 1, 1, DateTime.Now, DateTime.Now, 1);
+            List<Flight> list_of_flights = (List<Flight>)anonymousUserFacade.GetAllFlights();
+            List<Flight> expected_list_of_flights = null;
+            expected_list_of_flights.Add(expectedFlight);
+            Assert.AreEqual(list_of_flights, expected_list_of_flights);
+        }
+
+        [TestMethod]
+        public void GetFlightsByLandingDate()
+        {
+            Flight expectedFlight = new Flight(1, 1, 1, DateTime.Now, DateTime.Now, 1);
+            List<Flight> list_of_flights = (List<Flight>)anonymousUserFacade.GetAllFlights();
+            List<Flight> expected_list_of_flights = null;
+            expected_list_of_flights.Add(expectedFlight);
+            Assert.AreEqual(list_of_flights, expected_list_of_flights);
+        }
+
+        [TestMethod]
+        public void GetFlightsByOriginCountry()
+        {
+            Flight expectedFlight = new Flight(1, 1, 1, DateTime.Now, DateTime.Now, 1);
+            List<Flight> list_of_flights = (List<Flight>)anonymousUserFacade.GetAllFlights();
+            List<Flight> expected_list_of_flights = null;
+            expected_list_of_flights.Add(expectedFlight);
+            Assert.AreEqual(list_of_flights, expected_list_of_flights);
+        }
+
+        [TestMethod]
+        public void GetFlightsByDestinationCountry()
+        {
+            Flight expectedFlight = new Flight(1, 1, 1, DateTime.Now, DateTime.Now, 1);
+            List<Flight> list_of_flights = (List<Flight>)anonymousUserFacade.GetAllFlights();
+            List<Flight> expected_list_of_flights = null;
+            expected_list_of_flights.Add(expectedFlight);
+            Assert.AreEqual(list_of_flights, expected_list_of_flights);
+        }
+
     }
 }
