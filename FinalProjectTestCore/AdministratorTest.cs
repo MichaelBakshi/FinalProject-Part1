425ef125b51f105c21235ffd2017d06d9d7a9f86
Michael Bakshi <michael.bakshi83@gmail.com>
2021-04-05 13:49:32 +0300

    h

--- FinalProjectTestCore/AdministratorTest.cs
+++ FinalProjectTestCore/AdministratorTest.cs
@@ -10,6 +10,9 @@ namespace FinalProjectTestCore
     public class AdministratorTest
     {
         LoggedInAdministratorFacade administratorFacade;
+        LoggedInAirlineFacade airlineFacade;
+        AnonymousUserFacade anonymousUserFacade;
+        LoginToken<Administrator> admin_token;
 
         [TestInitialize]
         public void TryLogin()
@@ -19,5 +22,59 @@ namespace FinalProjectTestCore
             new LoginService().TryLogin("userName", "password", out loginToken);
             administratorFacade = FlightsCenterSystem.Instance.GetFacade(loginToken as LoginToken<Administrator>) as LoggedInAdministratorFacade;
         }
+
+        [TestMethod]
+        //[ExpectedException()]
+        public void CheckNullToken()
+        {
+            Assert.ThrowsException<Exception>(() =>
+            {
+                airlineFacade.CancelFlight(null, new Flight());
+            });
+            Assert.ThrowsException<Exception>(() =>
+            {
+                airlineFacade.GetAllTickets(null);
+            });
+            Assert.ThrowsException<Exception>(() =>
+            {
+                airlineFacade.CreateFlight(null, new Flight());
+            });
+            Assert.ThrowsException<Exception>(() =>
+            {
+                airlineFacade.UpdateFlight(null, new Flight());
+            });
+            Assert.ThrowsException<Exception>(() =>
+            {
+                airlineFacade.ChangeMyPassword(null, "old_password", "new_password");
+            });
+            Assert.ThrowsException<Exception>(() =>
+            {
+                airlineFacade.MofidyAirlineDetails(null, new AirlineCompany());
+            });
+        }
+
+
+
+        //get all customers
+        [TestMethod]
+        public void GetAllTickets()
+        {
+            Ticket expectedTicket = new Ticket(1, 1);
+            List<Ticket> list_of_tickets = (List<Ticket>)airlineFacade.GetAllTickets(airlineToken);
+            List<Ticket> expected_list_of_tickets = null;
+            expected_list_of_tickets.Add(expectedTicket);
+            Assert.AreEqual(list_of_tickets, expected_list_of_tickets);
+        }
+
+
+        //create new airline
+        //create new customer
+        //remove airline
+        //remove customer
+        //update airline details
+        //update customer details
+        //create admin
+        //update admin
+        //remove admin
     }
 }
