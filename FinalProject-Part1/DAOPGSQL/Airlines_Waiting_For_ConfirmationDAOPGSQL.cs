aa81081ae3ae5589f71ebbec503f380cf360a0e8
Michael Bakshi <michael.bakshi83@gmail.com>
2021-08-09 15:16:38 +0300

    f

--- FinalProject-Part1/DAOPGSQL/Airlines_Waiting_For_ConfirmationDAOPGSQL.cs
+++ FinalProject-Part1/DAOPGSQL/Airlines_Waiting_For_ConfirmationDAOPGSQL.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace FinalProject_Part1.DAOPGSQL
 {
-    class Airlines_Waiting_For_ConfirmationDAOPGSQL: IAirlineDAO
+    class Airlines_Waiting_For_ConfirmationDAOPGSQL
     {
         private string m_conn_string;
 
@@ -33,153 +33,19 @@ namespace FinalProject_Part1.DAOPGSQL
             return result;
         }
 
-
         public void Add(AirlineCompany a)
         {
 
-            ExecuteNonQuery($"call sp_insert_airline('{a.Name}', {a.Country_Id}, {a.User_Id});");
+            ExecuteNonQuery($"call sp_insert_airline_to_confirmation_list('{a.Name}', {a.Country_Id}, {a.User_Id});");
         }
-
-        public AirlineCompany GetById(int id)
-        {
-            AirlineCompany result = null;
-
-            using (NpgsqlCommand cmd = new NpgsqlCommand())
-            {
-                using (cmd.Connection = new NpgsqlConnection(m_conn_string))
-                {
-                    cmd.Connection.Open();
-                    cmd.CommandType = System.Data.CommandType.Text;
-                    cmd.CommandText = $"select * from sp_get_airline_by_id({id})";
-
-                    NpgsqlDataReader reader = cmd.ExecuteReader();
-
-                    if (reader.Read())
-                    {
-                        result = new AirlineCompany
-                        {
-                            Id = (int)reader["id"],
-                            Name = reader["name"].ToString(),
-                            Country_Id = (int)reader["country_id"],
-                            User_Id = (int)reader["user_id"]
-                        };
-                        result.user = new User
-                        {
-                            Id = (int)reader["id"],
-                            Username = reader["username"].ToString(),
-                            Password = reader["password"].ToString(),
-                            Email = reader["email"].ToString(),
-                            User_Role = (int)reader["user_role"]
-                        };
-                    }
-                }
-            }
 
             //UserDAOPGSQL userDAOPGSQL = new UserDAOPGSQL();
             //result.user = userDAOPGSQL.GetById(result.User_Id);
 
-
-            return result;
-        }
-
-        public AirlineCompany GetAirlineByUsername(string _username)
-        {
-            AirlineCompany result = null;
-
-            using (NpgsqlCommand cmd = new NpgsqlCommand())
-            {
-                using (cmd.Connection = new NpgsqlConnection(m_conn_string))
-                {
-                    cmd.Connection.Open();
-                    cmd.CommandType = System.Data.CommandType.Text;
-                    cmd.CommandText = $"select * from sp_get_airline_by_username('{_username}')";
-
-                    NpgsqlDataReader reader = cmd.ExecuteReader();
-
-                    if (reader.Read())
-                    {
-                        result = new AirlineCompany
-                        {
-                            Id = (int)reader["id"],
-                            Name = reader["name"].ToString(),
-                            Country_Id = (int)reader["country_id"],
-                            User_Id = (int)reader["user_id"],
-
-                        };
-                    }
-                }
-            }
-            return result;
-        }
-
-        public IList<AirlineCompany> GetAllAirlinesByCountry(int country_id)
-        {
-            List<AirlineCompany> result = new List<AirlineCompany>();
-
-            using (NpgsqlCommand cmd = new NpgsqlCommand())
-            {
-                using (cmd.Connection = new NpgsqlConnection(m_conn_string))
-                {
-                    cmd.Connection.Open();
-                    cmd.CommandType = System.Data.CommandType.Text;
-                    cmd.CommandText = $"select * from sp_get_all_airlines_by_country({country_id})";
-
-                    NpgsqlDataReader reader = cmd.ExecuteReader();
-
-                    while (reader.Read())
-                    {
-                        AirlineCompany c = new AirlineCompany
-                        {
-                            Id = (int)reader["id"],
-                            Name = reader["name"].ToString(),
-                            Country_Id = (int)reader["country_id"],
-                            User_Id = (int)reader["user_id"]
-                        };
-                        result.Add(c);
-                    }
-                }
-            }
-            return result;
-        }
-
-        public List<AirlineCompany> GetAll()
-        {
-            List<AirlineCompany> result = new List<AirlineCompany>();
-
-            using (NpgsqlCommand cmd = new NpgsqlCommand())
-            {
-                using (cmd.Connection = new NpgsqlConnection(m_conn_string))
-                {
-                    cmd.Connection.Open();
-                    cmd.CommandType = System.Data.CommandType.Text;
-                    cmd.CommandText = "select * from sp_get_all_airline_companies()";
-
-                    NpgsqlDataReader reader = cmd.ExecuteReader();
-
-                    while (reader.Read())
-                    {
-                        AirlineCompany c = new AirlineCompany
-                        {
-                            Id = (int)reader["id"],
-                            Name = reader["name"].ToString(),
-                            Country_Id = (int)reader["country_id"],
-                            User_Id = (int)reader["user_id"]
-                        };
-                        result.Add(c);
-                    }
-                }
-            }
-            return result;
-        }
-
         public void Remove(AirlineCompany airline)
         {
-            int result = ExecuteNonQuery($"call  sp_delete_airline_company ({airline.Id})");
+            int result = ExecuteNonQuery($"call  sp_delete_airline_company_from_confirmation_list ({airline.Id})");
         }
 
-        public void Update(AirlineCompany airline)
-        {
-            int result = ExecuteNonQuery($"call sp_update_airline( {airline.Id}, '{airline.Name}', {airline.Country_Id}, {airline.User_Id})");
-        }
     }
 }
