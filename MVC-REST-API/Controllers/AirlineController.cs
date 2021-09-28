bc4f15b9f46ee15aafbbed568b78c8d51103c208
Michael Bakshi <michael.bakshi83@gmail.com>
2021-09-28 16:52:27 +0300

    h

--- MVC-REST-API/Controllers/AirlineController.cs
+++ MVC-REST-API/Controllers/AirlineController.cs
@@ -281,6 +281,48 @@ namespace MVC_REST_API.Controllers
             return Ok(flight);
         }
 
+
+        //public async Task<ActionResult<List<FlightDTO>>> GetFlightsByAirline()
+        //{
+        //    AirlineCompanyProfile profile = new AirlineCompanyProfile();
+
+        //    AuthenticateAndGetTokenAndGetFacade(out LoginToken<AirlineCompany>
+        //            token_airline, out LoggedInAirlineFacade facade);
+
+        //    List<FlightDTO> result = null;
+        //    try
+        //    {
+
+        //        List<Flight> list = await Task.Run(() => facade.GetFlightsByAirline(token_airline)) as List<Flight>;
+        //        List<FlightDTO> flightDTOList = new List<FlightDTO>();
+
+        //        foreach (Flight flight in list)
+        //        {
+        //            //added our own m_mapper
+        //            FlightDTO flightDTO = m_mapper.Map<Flight, FlightDTO>(flight);
+
+        //            if (flight.Departure_Time.CompareTo(DateTime.Now) >= 0)   // 
+        //            {
+        //                flightDTO.IsCancellable = true;
+        //            }
+
+        //            flightDTOList.Add(flightDTO);
+        //        }
+        //        result = flightDTOList;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return StatusCode(400, $"{{ error: can't get all flights \"{ex.Message}\" }}");
+        //    }
+        //    if (result == null)
+        //    {
+        //        return StatusCode(204, "{The list is empty.}");
+        //    }
+        //    return Ok(result);
+        //}
+
+
+
         // PUT api/<AirlineController>/5
         [HttpPut("UpdateFlight")]
         public async Task<ActionResult> UpdateFlight([FromBody] Flight flight)
